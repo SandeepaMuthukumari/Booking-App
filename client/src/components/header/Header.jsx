ebcbcac2153813feaab21e91a3fff3b094071ee7
SandeepaMuthukumari <166691172+SandeepaMuthukumari@users.noreply.github.com>
2024-10-11 13:53:08 +0530

    second-commit

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -34,7 +34,7 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
-  const { user } = useContext(AuthContext);
+  //const { user } = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
